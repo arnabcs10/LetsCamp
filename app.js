fe6f7b1e39ff76675b66e4307ba3bd911728faa1
Arnab Sengupta <arnab10panda@gmail.com>
2020-05-09 22:40:54 +0530

    port modified

--- app.js
+++ app.js
@@ -68,7 +68,7 @@ app.use("/",indexRoutes);
 app.use("/campgrounds",campgroundRoutes);
 app.use("/campgrounds/:id/comments",commentRoutes);
 
-//PORT 3000
-app.listen(3000,function(){
+//PORT 
+app.listen(process.env.PORT,process.env.IP,function(){
   console.log("YelpCamp Server Started");
 });
